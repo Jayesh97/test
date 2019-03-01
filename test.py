6c41554b87c261191ae0a5261f71aafbc9dfdae0
Sai Jayesh <jayesh5397@gmail.com>
2019-03-01 11:33:37 +0530

    final

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 import os
+import yaml
+import sys
 
-print("Hello world")
-print(os.getcwd())
+
+def yaml_loader(filepath):
+    with open(filepath,'r+') as fy:
+        data = yaml.load(fy)
+    return data
+
+
 
 filepath = sys.argv[1]
 p = os.getcwd()+'/'+filepath
 head, filename = os.path.split(p)
 os.chdir(head)
-f1p = filepath
+f1p = p
 a1 = yaml_loader(f1p)
 print(a1)
