72cd949102b7b79e775c43684dc7ed4a4fee81f3
Sai Jayesh <jayesh5397@gmail.com>
2019-03-01 11:28:43 +0530

    c3

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ import os
 
 print("Hello world")
 print(os.getcwd())
+
+filepath = sys.argv[1]
+p = os.getcwd()+'/'+filepath
+head, filename = os.path.split(p)
+os.chdir(head)
+f1p = filepath
+a1 = yaml_loader(f1p)
+print(a1)
