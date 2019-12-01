2e37bbc970cfe36a847040a2a82d7829da54287b
Sai Jayesh <30596681+Jayesh97@users.noreply.github.com>
2019-11-30 20:23:11 -0500

    Update test_merge.py

--- test_merge.py
+++ test_merge.py
@@ -6,10 +6,10 @@ class Testmerge(unittest.TestCase):
     #Best_practice - Test cases can be read from a CSV file
     def test_merge(self):
         result = merge_logic.merge('testcases/test1/dir1/input.yaml')
-        self.assertEqual(result,{'color': 'blue', 'count': 2, 'size': 8})
+        self.assertEqual(result,{'color': 'blue', 'count': 2, 'size': 9})
 
         result = merge_logic.merge('testcases/test2/dir1/input.yaml')
-        self.assertEqual(result,{'wishlist': ['worldpeace', 'car', 'pony']})
+        self.assertEqual(result,{'wishlist': ['worldpeace', 'car', 'pony1']})
 
         result = merge_logic.merge('testcases/test3/dir1/input.yaml')
         self.assertEqual(result,{'todo': {'laundry': {'priority': 'low'}, 'dishes': {'priority': 'low'}, 'vacuum': {'priority': 'high'}}})
